8b1015def391b90a2f2ef2b5acabfea608446fcf
Stephen <kringz@gmail.com>
2016-04-29 16:35:48 -0400

    Added call to OpenFuck binary

--- openfuck-iterator/openfuck-iterator.py
+++ openfuck-iterator/openfuck-iterator.py
@@ -2,9 +2,11 @@
 
 from subprocess import call
 
+# openfuck loop script
+# Stephen Krings
+
 #hex(version)
 
-for i in xrange(137):
+for i in xrange(138):
 	form = "0x{:02x}".format(i)
-	print form
-#	call(["./OpenFuck", i, "192.168.15.244", "8000"])
+	call(["./OpenFuck", form, "192.168.15.244", "8000"])
