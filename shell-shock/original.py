841206417a519bc99e8a0a42ab6437294eb56647
Stephen <kringz@gmail.com>
2016-04-29 23:31:25 -0400

    .

--- shell-shock/original.py
+++ shell-shock/original.py
@@ -1,5 +1,6 @@
 
-import pxssh
+
+from pexpect import pxssh
 import getpass
 try:                                                            
     s = pxssh.pxssh()
@@ -7,15 +8,9 @@ try:
     username = raw_input('username: ')
     password = getpass.getpass('password: ')
     s.login (hostname, username, password)
-    s.sendline ('uptime')   # run a command
+    s.sendline ('uname')   # run a command
     s.prompt()             # match the prompt
     print s.before          # print everything before the prompt.
-    s.sendline ('ls -l')
-    s.prompt()
-    print s.before
-    s.sendline ('df')
-    s.prompt()
-    print s.before
     s.logout()
 except pxssh.ExceptionPxssh, e:
     print "pxssh failed on login."
